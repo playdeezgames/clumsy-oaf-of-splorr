8c455570d696314e65d900891e94317df89dd8f6
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-08-16 04:41:32 -0500

    i think we can get started.

---
+++ src/Metaphor.Extensions/InitializationContextExtensions.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Metaphor.Extensions
+Imports Metaphor.Persistence
+
+Friend Module InitializationContextExtensions
+    <Extension>
+    Friend Function InitializeBlueRoom(context As IInitializationContext) As Persistence.LocationInitializer
+        Return Sub(room)
+                   room.CreateN00b(context.ChosenName, context.InitializeAvatar())
+               End Sub
+    End Function
+    <Extension>
+    Private Function InitializeAvatar(context As IInitializationContext) As CharacterInitializer
+        Return Sub(character)
+                   character.World.Avatar = character
+               End Sub
+    End Function
+End Module
--- src/Metaphor.Extensions/WorldExtensions.vb
+++ src/Metaphor.Extensions/WorldExtensions.vb
@@ -1,2 +1,16 @@
-﻿Friend Module WorldExtensions
+﻿Imports System.Runtime.CompilerServices
+Imports Metaphor.Persistence
+
+Public Module WorldExtensions
+    <Extension>
+    Private Sub CreateBlueRoom(world As IWorld, context As IInitializationContext)
+        world.CreateLocation(LocationSubtypes.BLUE_ROOM, "The Blue Room", context.InitializeBlueRoom())
+    End Sub
+    <Extension>
+    Public Sub Initialize(world As IWorld, context As IInitializationContext)
+        world.Clear()
+        world.CreateBlueRoom(context)
+        world.AddMessage("Welcome to Clumsy Oaf of SPLORR!!")
+        world.Avatar.Look()
+    End Sub
 End Module
